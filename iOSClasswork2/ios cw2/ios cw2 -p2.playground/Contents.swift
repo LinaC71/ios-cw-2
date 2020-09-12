901bb788a5b1277c3f9bd84f47a467c38df9e13e
LinaC71 <Linaalosaimi155@gmail.com>
2020-09-12 22:24:52 +0300

    done?!

---
+++ iOSClasswork2/ios cw2/ios cw2 -p2.playground/Contents.swift
@@ -0,0 +1,12 @@
+var fruits = ["Mango", "Orange", "Potato", "Peach"]
+print(fruits [0])
+print(fruits [3])
+
+fruits.append("Strawberry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+fruits += ["Banana", "Watermelon", "Pineapple", "Kiwi"]
+print(fruits)
+fruits.removeAll()
+print(fruits)
---
+++ iOSClasswork2/ios cw2/ios-cw2 .playground/Contents.swift
@@ -0,0 +1,14 @@
+let family = ["Lina", "Rinad", "Hala"]
+var evens = [2,4,6,8,10,12]
+var grades = [95.1, 93.2]
+var trueFalse = [true, true, true, false]
+
+print("Family: ", family[0], family[2])
+print("Evens: ", evens[0], evens[5])
+print("Grades: ", grades[0], grades[1])
+print("TrueFalse: ", trueFalse[0], trueFalse[3])
+
+
+
+
+
